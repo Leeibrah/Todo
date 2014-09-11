83694b096c9e9d1acdf01a4723e58eb451eae916
Leeibrah <leeibrah@gmail.com>
2014-09-11 17:51:59 +0300

    All files

---
+++ app/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=utf8" />
+
+    <body class="bzw">
+    
+        Login to see your TODOS
+
+        <button>
+            <a href="/login">Login</a>
+        </button>
+    </body>
+
+<!-- Mirrored from www.Mafundi.com/ by HTTrack Website Copier/3.x [XR&CO'2013], Thu, 20 Feb 2014 19:02:05 GMT -->
+</html>
+
+<!-- end footer/foot-index.php -->
